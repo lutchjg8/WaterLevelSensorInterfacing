397cc149c5c5d59ffd897ef00f5b2f7197801536
lutchjg8 <71037249+lutchjg8@users.noreply.github.com>
2021-04-11 16:47:40 -0400

    Add files via upload

--- level.c
+++ level.c
@@ -1,72 +1,93 @@
 #include <avr/io.h>
 #include <util/delay.h>
-/* Change these values based on your calibration values */
-int lowerThreshold = 420;
-int upperThreshold = 520;
 
-// Sensor pins
-#define sensorPower 7
-#define sensorPin A0
+/* This is the Design By Contract macros.*/
+#define DBC // Can turn off these macros by commenting out this line
+#ifdef DBC
+/* needs to be at main since we are going to use Pin13 as our LED to warn us on assert fails */
+#define DBC_SETUP()                                                    \
+/* turn on Pin 13 as we will use to indicate assertion/error failed */ \
+DDRB |= _BV(DDB5); 
 
-// Value for storing water level
-int val = 0;
-
-// Declare pins to which LEDs are connected
-int redLED = 2;
-int yellowLED = 3;
-int greenLED = 4;
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(sensorPower, OUTPUT);
-  digitalWrite(sensorPower, LOW);
-  
-  // Set LED pins as an OUTPUT
-  pinMode(redLED, OUTPUT);
-  pinMode(yellowLED, OUTPUT);
-  pinMode(greenLED, OUTPUT);
-  
-  // Initially turn off all LEDs
-  digitalWrite(redLED, LOW);
-  digitalWrite(yellowLED, LOW);
-  digitalWrite(greenLED, LOW);
+#define PRE_CONDITION_DBC(eval_expression, time_blink_delay) \
+while (!(eval_expression))                                   \
+{                                                            \
+  PORTB |= _BV(PORTB5);                                      \
+  my_delay_ms(time_blink_delay);                             \
+  PORTB &= ~_BV(PORTB5);                                     \
+  my_delay_ms(time_blink_delay);                             \
 }
 
-void loop() {
-  int level = readSensor();
-  
-  if (level == 0) {
-    Serial.println("Water Level: Empty");
-    digitalWrite(redLED, LOW);
-    digitalWrite(yellowLED, LOW);
-    digitalWrite(greenLED, LOW);
+#define POST_CONDITION_DBC(eval_expression, time_blink_delay) \
+while (!(eval_expression))                                    \
+{                                                             \
+  PORTB |= _BV(PORTB5);                                       \
+  my_delay_ms(time_blink_delay);                              \
+  PORTB &= ~_BV(PORTB5);                                      \
+  /* half the delay off on post condition */                  \
+  my_delay_ms(time_blink_delay/2);                            \
+}
+#else
+/* These are empty for when turned off */
+#define DBC_SETUP() {}
+#define PRE_CONDITION(eval_expression, time_blink_delay) {}
+#define POST_CONDITION(eval_expression, time_blink_delay) {}
+#endif
+
+/* C Prototypes of functions */
+void my_delay_ms( unsigned int t);
+short readSensor(int a);
+int lowerThresh = 100;
+int upperThresh = 450;
+
+int main (void) 
+{
+  DBC_SETUP();
+  DDRD |= _BV(DDD2);
+  DDRD |= _BV(DDD3);
+  DDRD |= _BV(DDD4);
+  DDRD |= _BV(DDD7);
+
+  while(1) 
+  {
+    PORTD |= _BV(PORTD7);
+    int val = readSensor(PC0);
+    if (val < lowerThresh)
+    {
+      PORTD |= _BV(PORTD2);
+    }
+    if(val > lowerThresh & val < upperThresh)
+    {
+      PORTD |= _BV(PORTD3);
+    }
+    if(val > upperThresh)
+    {
+      PORTD |= _BV(PORTD4);
+    }
   }
-  else if (level > 0 && level <= lowerThreshold) {
-    Serial.println("Water Level: Low");
-    digitalWrite(redLED, HIGH);
-    digitalWrite(yellowLED, LOW);
-    digitalWrite(greenLED, LOW);
-  }
-  else if (level > lowerThreshold && level <= upperThreshold) {
-    Serial.println("Water Level: Medium");
-    digitalWrite(redLED, LOW);
-    digitalWrite(yellowLED, HIGH);
-    digitalWrite(greenLED, LOW);
-  }
-  else if (level > upperThreshold) {
-    Serial.println("Water Level: High");
-    digitalWrite(redLED, LOW);
-    digitalWrite(yellowLED, LOW);
-    digitalWrite(greenLED, HIGH);
-  }
-  delay(1000);
+}
+short readSensor(int a)
+{
+  int b = a;
+  return b;
 }
 
-//This is a function used to get the reading
-int readSensor() {
-  digitalWrite(sensorPower, HIGH);
-  delay(10);
-  val = analogRead(sensorPin);
-  digitalWrite(sensorPower, LOW);
-  return val;
+
+/* 
+* Handles larger delays the _delay_ms can't do by itself (not sure how accurate)  
+* Note no DBC as this function is used in the DBC !!! 
+*
+* borrowed from : https://www.avrfreaks.net/forum/delayms-problem 
+* */
+void my_delay_ms(unsigned int t) 
+{
+  unsigned int i;
+  
+  for (i=0; i<(t/10); i++) 
+  {
+    _delay_ms(10);
+  }
+  if (t % 10) {
+    _delay_ms(10);
+  }
 }
